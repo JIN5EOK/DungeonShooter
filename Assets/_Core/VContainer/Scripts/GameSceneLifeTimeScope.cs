477d120bc2ca854c27e25937b3665f0d7c7d8fb1
JIN5EOK <baejinseok98@gmail.com>
2026-01-12 16:02:07 +0900

    docs: 씬 전환 구조 설계 구상중

--- Assets/_Core/VContainer/Scripts/GameSceneLifeTimeScope.cs
+++ Assets/_Core/VContainer/Scripts/GameSceneLifeTimeScope.cs
@@ -4,10 +4,10 @@ using VContainer.Unity;
 namespace DungeonShooter
 {
     /// <summary>
-    /// 인게임에서 사용되는 서비스들을 등록하는 라이프타임 스코프
+    /// 게임 씬에서 사용되는 서비스들을 등록하는 라이프타임 스코프
     /// Bootstrap에서 EnqueueParent로 등록된 GlobalLifeTimeScope를 자동으로 부모로 사용
     /// </summary>
-    public class GameLifeTimeScope : LifetimeScope
+    public class GameSceneLifeTimeScope : LifetimeScope
     {
         protected override void Configure(IContainerBuilder builder)
         {
@@ -15,7 +15,6 @@ namespace DungeonShooter
             builder.Register<Inventory>(Lifetime.Singleton);
             builder.Register<EntityFactory>(Lifetime.Singleton);
             builder.RegisterEntryPoint<SceneStartInjector>();
-            
             base.Configure(builder);
         }
     }
