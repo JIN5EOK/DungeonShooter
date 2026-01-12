f9a4350be46d4510a63a5af657553b12f4bbc655
JIN5EOK <baejinseok98@gmail.com>
2026-01-12 16:47:10 +0900

    feat: SceneLoader 기능 구현

---
+++ Assets/_Core/VContainer/Scripts/SceneLoader.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using VContainer;
+using VContainer.Unity;
+
+namespace DungeonShooter
+{
+    /// <summary>
+    /// 씬을 로드할 때 사용하는 일회성 객체
+    /// 컨텍스트를 추가한 후 LoadScene을 호출하면 하나의 LifetimeScope.Enqueue 블록에서 모든 컨텍스트를 등록한 후 씬을 로드한다
+    /// </summary>
+    public class SceneLoader
+    {
+        private readonly List<Action<IContainerBuilder>> _contextRegistrations = new();
+
+        /// <summary>
+        /// 로드할 씬의 LifeTimeScope에 반영할 Context를 추가한다
+        /// </summary>
+        /// <typeparam name="T">컨텍스트 타입</typeparam>
+        /// <param name="context">추가할 컨텍스트</param>
+        /// <returns>메서드 체이닝을 위한 SceneLoader 인스턴스</returns>
+        public SceneLoader AddContext<T>(T context) where T : class
+        {
+            _contextRegistrations.Add(builder => builder.RegisterInstance(context));
+            return this;
+        }
+
+        /// <summary>
+        /// Addressables를 통해 비동기로 씬을 로드한다
+        /// 내부적으로 LifetimeScope.Enqueue 블록을 생성하여 이전에 AddContext로 추가한 모든 컨텍스트를 등록한 후 씬을 로드한다
+        /// </summary>
+        /// <param name="sceneName">로드할 씬 이름</param>
+        public async Awaitable LoadScene(string sceneName)
+        {
+            if (string.IsNullOrEmpty(sceneName))
+            {
+                Debug.LogError($"[{nameof(SceneLoader)}] 씬 이름이 비었습니다.");
+                return;
+            }
+
+            using (LifetimeScope.Enqueue(builder =>
+            {
+                foreach (var registration in _contextRegistrations)
+                {
+                    registration?.Invoke(builder);
+                }
+            }))
+            {
+                try
+                {
+                    var handle = Addressables.LoadSceneAsync(sceneName);
+                    await handle.Task;
+
+                    if (handle.Status != AsyncOperationStatus.Succeeded)
+                    {
+                        Debug.LogError($"[{nameof(SceneLoader)}] 씬 로드에 실패했습니다: {sceneName}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"[{nameof(SceneLoader)}] 씬 로드에 실패했습니다: {ex.Message}");
+                    throw;
+                }
+            }
+        }
+    }
+}
