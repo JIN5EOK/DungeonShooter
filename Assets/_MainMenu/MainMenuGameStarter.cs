59d1b70af2b69ced27673bc088766f70c2d83973
JIN5EOK <baejinseok98@gmail.com>
2026-01-30 19:44:28 +0900

    fix: 유니티 API를 메인스레드 외에서 호출해 발생하는 버그 수정

--- Assets/_MainMenu/MainMenuGameStarter.cs
+++ Assets/_MainMenu/MainMenuGameStarter.cs
@@ -5,19 +5,13 @@ using VContainer.Unity;
 
 public class MainMenuGameStarter : IStartable
 {
-    // TODO: 테스트용 코드로 개선 필요함
-    [Inject]
-    private async void Construct(ITableRepository tableRepository)
+    public async void Start()
     {
         var loader = new SceneLoader();
         // TODO: 실제 플레이어 선택 및 스테이지 선택 로직으로 대체 필요
         var context = new StageContext(12000000, 13000001); // playerConfigTableId, stageConfigTableId
         await loader
-        .AddContext(context)
-        .LoadScene("StageScene");
-    }
-
-    public void Start()
-    {
+            .AddContext(context)
+            .LoadScene("StageScene");
     }
 }
