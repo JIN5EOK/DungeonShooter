b016d07904cd2ebcd656b5db6fbc7a14173d45cc
JIN5EOK <baejinseok98@gmail.com>
2026-04-07 17:03:57 +0900

    test: 테스트 환경 수정

--- Assets/_Test/PerformanceTestInitializer.cs
+++ Assets/_Test/PerformanceTestInitializer.cs
@@ -1,4 +1,5 @@
 using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using VContainer;
 using Random = UnityEngine.Random;
@@ -11,37 +12,53 @@ namespace DungeonShooter
         private EntityBase _player;
         private IEnemyFactory _enemyFactory;
         private IPlayerFactory _playerFactory;
-        
+        private IPlayerContextManager _playerContextManager;
+        private IItemFactory _itemFactory;
+        private IInventory _inventory;
         [Inject]
-        public void Construct(IPlayerContextManager playerContextManager, IPlayerFactory playerFactory, IEventBus eventBus, IEnemyFactory enemyFactory)
+        public void Construct(IPlayerContextManager playerContextManager, IPlayerFactory playerFactory, IEventBus eventBus, IEnemyFactory enemyFactory, IItemFactory itemFactory,  IInventory inventory)
         {
+            _itemFactory = itemFactory;
+            _inventory = inventory;
+            _playerContextManager = playerContextManager;
             playerContextManager.Initialize(12000001);
             _playerFactory = playerFactory;    
             eventBus.Subscribe<PlayerObjectSpawnEvent>(OnPlayerSpawned);
             _enemyFactory = enemyFactory;
         }
 
-        public void Start()
+        public async void Start()
         {
-            _playerFactory.GetPlayerAsync(12000001, Vector2.zero);
+            await _playerFactory.GetPlayerAsync(12000001, Vector2.zero);
+            await _playerContextManager.InitializeSkillsAsync();
             
-            OnPlayerSpawned(new PlayerObjectSpawnEvent());
+            var weapon = await _itemFactory.CreateItemAsync(15000001);
+            _inventory.AddItem(weapon);
+            _inventory.EquipItem(weapon);
+            await SpawnEnemy();
         }
         
-        private async void OnPlayerSpawned(PlayerObjectSpawnEvent ev)
+        private void OnPlayerSpawned(PlayerObjectSpawnEvent ev)
         {
             _player = ev.player;
+            
+            
 
-            var radius = 5;
+             
+        }
+
+        private async UniTask SpawnEnemy()
+        {
+            var radius = 10;
             
             
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < 300; i++)
             {
                 var angle = Random.Range(0f, Mathf.PI * 2);
                 
                 var x = Mathf.Cos(angle) * radius;
                 var y = Mathf.Sin(angle) * radius;
-                await _enemyFactory.GetEnemyByConfigIdAsync(18000000, _player.transform.position + new Vector3(x, y, 1));
+                await _enemyFactory.GetEnemyByConfigIdAsync(18000002, _player.transform.position + new Vector3(x, y, 1));
             }
         }
     }
