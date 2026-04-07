b9d6595383d8338774c10541fe1a79dce7c8b002
JIN5EOK <baejinseok98@gmail.com>
2026-04-07 13:55:37 +0900

    style: 함수 비공개로 변경

--- Assets/_Core/Bootstrap.cs
+++ Assets/_Core/Bootstrap.cs
@@ -7,17 +7,17 @@ namespace DungeonShooter
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void Initialize()
         {
-            DevelopmentInit();
+            // 개발빌드 혹은 에디터 환경에서만 띄우기
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+                FPSOverlayInit();
+#endif
         }
 
-        public static void DevelopmentInit()
+        private static void FPSOverlayInit()
         {
-            // 개발빌드 혹은 에디터 환경에서만 띄우기
-            #if DEVELOPMENT_BUILD || UNITY_EDITOR
-                var fpsOverlay = new GameObject(nameof(FpsOverlay));
-                fpsOverlay.AddComponent<FpsOverlay>();
-                Object.DontDestroyOnLoad(fpsOverlay.gameObject);
-            #endif
+            var fpsOverlay = new GameObject(nameof(FpsOverlay));
+            fpsOverlay.AddComponent<FpsOverlay>();
+            Object.DontDestroyOnLoad(fpsOverlay.gameObject);
         }
     }
 }
