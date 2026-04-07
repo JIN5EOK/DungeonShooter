b611816085ae20a2aa8a81364c5d0c877d5fd15b
JIN5EOK <baejinseok98@gmail.com>
2026-04-07 16:23:13 +0900

    test: 테스트 스크립트 수정

--- Assets/_Test/PerformanceTestInitializer.cs
+++ Assets/_Test/PerformanceTestInitializer.cs
@@ -5,18 +5,29 @@ using Random = UnityEngine.Random;
 
 namespace DungeonShooter
 {
-    public class VPSurvivalEnemySpawner : MonoBehaviour
+    public class PerformanceTestInitializer : MonoBehaviour
     {
         private float _timer;
         private EntityBase _player;
         private IEnemyFactory _enemyFactory;
+        private IPlayerFactory _playerFactory;
+        
         [Inject]
-        public void Construct(IEventBus eventBus, IEnemyFactory enemyFactory)
+        public void Construct(IPlayerContextManager playerContextManager, IPlayerFactory playerFactory, IEventBus eventBus, IEnemyFactory enemyFactory)
         {
+            playerContextManager.Initialize(12000001);
+            _playerFactory = playerFactory;    
             eventBus.Subscribe<PlayerObjectSpawnEvent>(OnPlayerSpawned);
             _enemyFactory = enemyFactory;
         }
 
+        public void Start()
+        {
+            _playerFactory.GetPlayerAsync(12000001, Vector2.zero);
+            
+            OnPlayerSpawned(new PlayerObjectSpawnEvent());
+        }
+        
         private async void OnPlayerSpawned(PlayerObjectSpawnEvent ev)
         {
             _player = ev.player;
@@ -24,7 +35,7 @@ namespace DungeonShooter
             var radius = 5;
             
             
-            for (int i = 0; i < 300; i++)
+            for (int i = 0; i < 50; i++)
             {
                 var angle = Random.Range(0f, Mathf.PI * 2);
                 
