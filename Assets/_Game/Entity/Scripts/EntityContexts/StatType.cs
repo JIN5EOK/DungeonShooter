620ccbfb4e7cf310e8fe1fc00f422d7eb56fdb22
JIN5EOK <baejinseok98@gmail.com>
2026-04-20 17:40:35 +0900

    style: 폴더 정리

--- Assets/_Game/Entity/Scripts/EntityContexts/StatType.cs
+++ Assets/_Game/Entity/Scripts/EntityContexts/StatType.cs
@@ -1,6 +1,17 @@
 namespace DungeonShooter
 {
     /// <summary>
+    /// Entity 스탯 타입
+    /// </summary>
+    public enum StatType
+    {
+        Hp, // 최대 체력
+        Attack,
+        Defense,
+        MoveSpeed
+    }
+
+    /// <summary>
     /// 스탯 변경 데이터 타입 (Constant: 기본수치, Add: 더하기, Multiply: 곱하기)
     /// </summary>
     public enum StatModifierType
